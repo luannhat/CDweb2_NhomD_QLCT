356cbaecf084db7b404c4f62f6704d1f6970a592
nguyennguyen777 <hanhnguyenhpt1502@gmail.com>
2025-12-06 15:36:21 +0700

    xử lý hiển thị trang khoản thu trong dashboard và nút thêm khoản thu

--- sources/qlct/views/user/layout.php
+++ sources/qlct/views/user/layout.php
@@ -5,8 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý chi tiêu</title>
-    <link rel="stylesheet" href="./public/css/user.css">
+    <link rel="stylesheet" href="/public/css/user.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <?php if (!empty($cssFiles)): ?>
+        <?php foreach ($cssFiles as $css): ?>
+            <link rel="stylesheet" href="<?= $css ?>">
+        <?php endforeach; ?>
+    <?php endif; ?>
 
 </head>
 
